373a86ac7e68fa0c14adc26a4dd16c7eaa4fbbe1
carabria <studyingrhystic@gmail.com>
2023-12-04 10:43:31 -0500

    sql database

--- dotnet/database/capstone.sql
+++ dotnet/database/capstone.sql
@@ -20,12 +20,13 @@ CREATE TABLE users (
 	username varchar(50) NOT NULL,
 	password_hash varchar(200) NOT NULL,
 	salt varchar(200) NOT NULL,
-	user_role varchar(50) NOT NULL
+	user_role varchar(50) NOT NULL,
+	has_one_time_password bit NOT NULL
 	CONSTRAINT PK_user PRIMARY KEY (user_id)
 )
 
 --populate default data
-INSERT INTO users (username, password_hash, salt, user_role) VALUES ('user','Jg45HuwT7PZkfuKTz6IB90CtWY4=','LHxP4Xh7bN0=','user');
-INSERT INTO users (username, password_hash, salt, user_role) VALUES ('admin','YhyGVQ+Ch69n4JMBncM4lNF/i9s=', 'Ar/aB2thQTI=','admin');
+INSERT INTO users (username, password_hash, salt, user_role, has_one_time_password) VALUES ('user','Jg45HuwT7PZkfuKTz6IB90CtWY4=','LHxP4Xh7bN0=','user', 0);
+INSERT INTO users (username, password_hash, salt, user_role, has_one_time_password) VALUES ('admin','YhyGVQ+Ch69n4JMBncM4lNF/i9s=', 'Ar/aB2thQTI=','admin',0);
 
 GO

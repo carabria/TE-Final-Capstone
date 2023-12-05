b942bd986de9d4314802a0032f87fd5120a89b8f
Anthony Anderson <aantony4122@gmail.com>
2023-12-05 11:58:08 -0500

    Got User Selection working
    Signed-off-by: Anthony Anderson <aantony4122@gmail.com>

--- vue/src/components/UserSearchComponent.vue
+++ vue/src/components/UserSearchComponent.vue
@@ -1,6 +1,6 @@
 <template>
     <div>
-        <h2>User Search</h2>
+      <h2>User Search</h2>
 
       <table>      
         <thead>
@@ -15,16 +15,14 @@
           <td><input type="text" id="filter_name" v-model="search.username" placeholder="Username"></td>
           <td><input type="text" id="filter_role" v-model="search.role" placeholder="Role"></td>
         </tbody>
-        <tbody v-for="user in user_list" :key="user.userId">
+        <tbody v-for="user in filteredList" :key="user.userId">
           <tr>
             <td>{{ user.userId }}</td>
             <td>{{ user.username }}</td>
             <td>{{ user.role }}</td>
           </tr>
         </tbody>
-      </table>
-
-        
+      </table> 
     </div>
 </template>
 
@@ -46,6 +44,9 @@ export default {
       user_list: [],
     }
   },
+  created() {
+    this.getUsers();
+  },
   methods: {
     getUsers() {
       let session_token = this.$store.state.token;
@@ -60,12 +61,26 @@ export default {
     },
   },
   computed: {
-    current_users() {
-      let users = this.getUsers();
-      console.log(users);
-      return users;
-    }
-  },
+    filteredList() {
+      let user_filter = this.user_list; 
+      const { userId, username, role } = this.search;
+
+      if (userId != '') {
+        user_filter = user_filter.filter(user => user.userId == userId);
+      }
+
+      if (username != '') {
+        user_filter = user_filter.filter(user =>  user.username.toLowerCase().includes(username.toLowerCase()));
+      }
+
+      if (role != '') {
+        user_filter = user_filter.filter(user => user.role.toLowerCase().includes(role.toLowerCase()));
+      }
+
+      return user_filter
+   
+    },
+  }
 }
 </script>
 

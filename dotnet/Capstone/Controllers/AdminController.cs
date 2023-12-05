adb716bdc96a64dd6ddde619bce5d0f2122255e9
carabria <studyingrhystic@gmail.com>
2023-12-05 09:19:14 -0500

    admin controller made

--- dotnet/Capstone/Controllers/AdminController.cs
+++ dotnet/Capstone/Controllers/AdminController.cs
@@ -1,6 +1,39 @@
-﻿namespace Capstone.Controllers
+﻿using Microsoft.AspNetCore.Mvc;
+using Capstone.DAO;
+using Capstone.Exceptions;
+using Capstone.Models;
+using Capstone.Security;
+using System;
+using Microsoft.AspNetCore.Authorization;
+
+namespace Capstone.Controllers
 {
-    public class AdminController
+    [Route("[Controller]")]
+    [ApiController]
+    public class AdminController : ControllerBase
     {
+        private readonly IUserDao userDao;
+
+        public AdminController(IUserDao userDao)
+        {
+            this.userDao = userDao;
+        }
+
+        [Authorize(Roles = "admin")]
+        [HttpPut("/admin/resetpassword/{id}")]
+        public IActionResult resetPassword(int id)
+        {
+            User user = userDao.GetUserById(id);
+            string oneTimePassword;
+            try
+            {
+                oneTimePassword = userDao.GenerateOneTimePassword(user.Username);
+            }
+            catch (DaoException)
+            {
+                return StatusCode(500, "An internal server error occured.");
+            }
+            return Created("/admin/resetpassword", oneTimePassword);
+        }
     }
 }

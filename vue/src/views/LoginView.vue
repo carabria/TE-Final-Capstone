cca151e46d6bad6bda9eb486a4a1f8f086d1670b
Yukon Colegrove-Winkfield <1003597ywinkfield@gmail.com>
2023-12-05 20:48:31 -0500

    update

--- vue/src/views/LoginView.vue
+++ vue/src/views/LoginView.vue
@@ -1,24 +1,25 @@
 <template>
   <div id="login">
-    <form v-on:submit.prevent="login" id="loginForm">
-      <h1 >Please Sign In</h1>
-      <div role="alert" v-if="invalidCredentials">
+    <form v-on:submit.prevent="login" id="login-form">
+      <h1 id="login-prompt">Please Sign In</h1>
+      <div id="login-alert-invalid" role="alert" v-if="invalidCredentials">
         Invalid username and password!
       </div>
-      <div role="alert" v-if="this.$route.query.registration">
+      <div id="login-alert-success" role="alert" v-if="this.$route.query.registration">
         Thank you for registering, please sign in.
       </div>
       <div class="form-input-group" id="login-username">
-        <label for="username">Username</label>
+        <label id="username-label" for="username">Username</label>
         <input type="text" id="username" v-model="user.username" required autofocus />
       </div>
       <div class="form-input-group" id="login-password">
-        <label for="password">Password</label>
+        <label id="password-label" for="password">Password</label>
         <input type="password" id="password" v-model="user.password" required />
       </div>
       <button type="submit" id="login-submit">Sign in</button>
-      <p>
-      <router-link v-bind:to="{ name: 'register' }" id="login-register">Need an account? Sign up.</router-link></p>
+      <p id="login-register">
+        <router-link v-bind:to="{ name: 'register' }">Need an account? Sign up.</router-link>
+      </p>
     </form>
   </div>
 </template>

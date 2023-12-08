3be7d1ea6033a27078ab7eb4cfc8e1c78a6b97ed
carabria <studyingrhystic@gmail.com>
2023-12-08 10:54:32 -0500

    css changes

--- vue/src/App.vue
+++ vue/src/App.vue
@@ -3,15 +3,10 @@
     <div id="nav">
       &nbsp;
       <router-link class="navItem" v-bind:to="{ name: 'home' }">Home</router-link>
-      <div class="spacer"> | </div>
       <router-link class="navItem" v-bind:to="{name: 'user'}">User</router-link>
-      <div class="spacer"> | </div>
       <router-link class="navItem" v-bind:to="{name: 'protein'}">Protein</router-link>
-      <div class="spacer"> | </div>
       <router-link class="navItem" v-if="$store.state.user.role == 'admin'" v-bind:to="{name: 'admin'}">Admin</router-link>
-      <div class="spacer" v-if="$store.state.user.role == 'admin'"> | </div>
       <router-link class="navItem" v-bind:to="{ name: 'logout' }" v-if="$store.state.token != ''">Logout</router-link>
-      <div class="spacer" v-if="$store.state.token != ''"> | </div>
       <router-link class="navItem" v-bind:to="{ name: 'login' }" v-if="$store.state.token == ''">Login</router-link>
       &nbsp;
     </div>
@@ -24,15 +19,21 @@
 <style>
 
 div#nav {
+  font-family: sans-serif;
+  position:sticky;
+  top: 0px;
   display: flex;
   flex-direction: row;
   justify-content: center;
   align-items: center;
+  gap: 8vw;
   background-color: #333;
-  margin-left: auto;
-  margin-right: auto;
+  border-radius: 0%;
+  width: 100vw;
   color: white;
-  padding: 10px;
+  padding-top: 10px;
+  padding-bottom: 10px;
+  margin-bottom: 10px;
   text-align: center;
   font-size: 20px;
 }

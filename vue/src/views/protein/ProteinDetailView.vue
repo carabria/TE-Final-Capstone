4738f39b12695d183a25d3a93ceb2a307f2351b6
Neil De La Cruz <studyingrhystic@gmail.com>
2023-12-13 11:12:46 -0500

    css changes in protein detail view

--- vue/src/views/protein/ProteinDetailView.vue
+++ vue/src/views/protein/ProteinDetailView.vue
@@ -1,34 +1,38 @@
 <template>
-  <div class = "card">
+  <div class="card">
     <div class="top-bar">
-      <div class="card-header">
-        <h2>Protein Details</h2>
-      </div>
-      <div class = "p-name">
-        <h2>{{ protein.sequenceName }}</h2>
+      <div class="p-name">
+        <h1>{{ protein.sequenceName }}</h1>
       </div>
     </div>
-    <div class = "p-description">
+    <div class="p-description">
       <h2>{{ protein.description }}</h2>
     </div>
-    <div class = "p-sequence">
+    <div class="p-sequence">
       <h2>{{ protein.proteinSequence }}</h2>
     </div>
-      <form class = "p-generate" @submit.prevent="generateSequences()">
-        <button id="submit" type="submit" >Generate Sequences</button>
-      </form>
     <div class="generatedCard" v-show="newProtein.sequence1 !== null">
-      <div class = "sequence1">
-        <h2><a class= "blue">{{ newProtein.sequence1 }}</a>{{ protein.proteinSequence.substring(2, 6) }}...</h2>
+      <div class="card-labels">
+        <h3><a class="fast">Fast</a></h3>
+        <h3><a class="medium">Medium</a></h3>
+        <h3><a class="slow">Slow</a></h3>
       </div>
-      <div class = "sequence2">
-        <h2><a class= "green">{{ newProtein.sequence2 }}</a>{{ protein.proteinSequence.substring(2, 6) }}...</h2>
-      </div>
-      <div class = "sequence3">
-        <h2><a class= "yellow">{{ newProtein.sequence3 }}</a>{{ protein.proteinSequence.substring(2, 6) }}...</h2>
+      <div class="sequences">
+        <div class="sequence1">
+          <h2><a class="blue">{{ newProtein.sequence1 }}</a>{{ protein.proteinSequence.substring(2, 6) }}...</h2>
+        </div>
+        <div class="sequence2">
+          <h2><a class="green">{{ newProtein.sequence2 }}</a>{{ protein.proteinSequence.substring(2, 6) }}...</h2>
+        </div>
+        <div class="sequence3">
+          <h2><a class="yellow">{{ newProtein.sequence3 }}</a>{{ protein.proteinSequence.substring(2, 6) }}...</h2>
+        </div>
       </div>
     </div>
   </div>
+  <form class="p-generate" @submit.prevent="generateSequences()">
+    <button id="submit" type="submit">Generate Sequences</button>
+  </form>
 </template>
 
 <script>
@@ -48,9 +52,9 @@ export default {
         sequence3: ''
       },
       newProtein: {
-        sequence1: '',
-        sequence2: '',
-        sequence3: ''
+        blue: [],
+        green: [],
+        yellow: []
       }
     };
   },
@@ -76,14 +80,11 @@ export default {
         });
     },
     generateSequences() {
-        const token = this.$store.state.token;
-        const protein_id = this.$route.params.id;
-        GenerateService.generateSequences(token, protein_id)
+      const token = this.$store.state.token;
+      const protein_id = this.$route.params.id;
+      GenerateService.generateSequences(token, protein_id)
         .then(response => {
           this.protein = response.data;
-          this.newProtein.sequence1 = this.protein.sequence1.substring(0, 2);
-          this.newProtein.sequence2 = this.protein.sequence2.substring(0, 2);
-          this.newProtein.sequence3 = this.protein.sequence3.substring(0, 2);
         })
     }
   },
@@ -106,8 +107,7 @@ body {
   height: fit-content;
   margin-left: auto;
   margin-right: auto;
-  width: 70%;
-  height: 90%;
+  width: 70vw;
   color: black;
   border-radius: 2.5vh;
   background-color: aliceblue;
@@ -118,29 +118,24 @@ body {
   display: grid;
   grid-template-columns: 1fr 1fr;
   align-items: center;
-  grid-template-areas: 
-  "top-bar top-bar"
-  "p-description p-description"
-  "p-sequence p-sequence";
+  grid-template-areas:
+    "top-bar top-bar"
+    "p-description p-description"
+    "p-sequence p-sequence";
 }
 
 .top-bar {
   margin: auto;
   grid-area: top-bar;
   grid-template-columns: 1fr;
-  grid-template-areas: 
-  "card-header p-name";
-}
-.card-header {
-  margin-right: 10px;
-  grid-area: card-header;
-  display: inline-block;  
+  grid-template-areas:
+    "p-name";
 }
 
 .p-name {
-  margin-left: 10px;
   grid-area: p-name;
   display: inline-block;
+  font-weight: bold;
 }
 
 .p-description {
@@ -153,7 +148,12 @@ body {
   grid-area: p-sequence;
   word-break: break-all;
   margin-left: 25px;
-  margin-right: 25px;
+  margin-right: 50px;
+  border: black 2px;
+  border-left: 0px;
+  border-right: 0px;
+  border-style: solid;
+  margin-bottom: 20px;
 }
 
 #submit {
@@ -163,27 +163,94 @@ body {
   justify-self: right;
 }
 
+.generatedCard {
+  display: grid;
+  justify-content: center;
+  align-items: center;
+  width: 70vw;
+  color: black;
+  background-color: aliceblue;
+}
+
+.card-labels {
+  display: inline-flex;
+  flex-direction: row;
+  justify-content: center;
+  align-items: center;
+  width: 70vw;
+  margin: auto;
+}
+
+.fast {
+  color: black;
+  margin-right: auto;
+  word-wrap: break-word;
+  width: 33%;
+}
+
+.medium {
+  color: black;
+  word-wrap: break-word;
+  width: 33%;
+}
+
+.slow {
+  color: black;
+  margin-left: auto;
+  word-wrap: break-word;
+  width: 33%;
+}
+
+.sequences {
+  display: inline-flex;
+  border: black 2px;
+  border-right: 0px;
+  border-left: 0px;
+  border-bottom: 0px;
+  border-style: solid;
+  width: 100%;
+}
+
 .sequence1 {
   word-wrap: break-word;
+  margin-right: auto;
+  border: black 3px;
+  border-bottom: 0px;
+  border-left: 0px;
+  border-top: 0px;
+  border-style: solid;
+  background-color: rgba(0, 0, 255, .5);
+  width: 33.33%;
+  overflow: hidden;
 }
 
 .sequence2 {
   word-wrap: break-word;
+  background-color: rgba(0, 128, 0, .5);
+  border: black 3px;
+  border-bottom: 0px;
+  border-left: 0px;
+  border-top: 0px;
+  border-style: solid;
+  width: 33.33%;
 }
 
 .sequence3 {
   word-wrap: break-word;
+  margin-left: auto;
+  background-color: rgba(255, 255, 0, .5);
+  width: 33.33%;
 }
 
 .blue {
-  color: blue;
+  color: black;
 }
 
 .green {
-  color: green;
+  color: black;
 }
 
 .yellow {
-  color: yellow;
+  color: black;
 }
 </style>

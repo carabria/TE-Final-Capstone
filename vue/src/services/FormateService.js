3af7ff48d3bc5da43307e5a7ee9544a1b83188d1
Anita Anderson <anita@ansan.io>
2023-12-13 11:45:36 -0500

    Added the format converter
    Signed-off-by: Anita Anderson <anita@ansan.io>

---
+++ vue/src/services/FormateService.js
@@ -0,0 +1,52 @@
+export default {
+
+  convert_fromat: function (format_type, data) {
+    if (format_type === 1) {
+      return this.___convert_to_common_format(data);
+    } else if (format_type === 2) {
+      return this.___convert_to_space_format(data);
+    } else if (format_type === 3) {
+      return this.___convert_to_number_format(data);
+    } else {
+      return 'Invalid format type';
+    }
+
+  },
+
+  /**
+   * also known as format type 1
+   * @param {string} data
+   * @return {string}
+   */
+  ___convert_to_common_format: function(data) {
+    //replace numbers and spaces with empty string
+    return data.replace(/[^a-zA-Z]/g, '');
+  },
+
+
+  /**
+   * This is known as format type 2
+   * @param {string} data
+   * @return {string}
+   */
+  ___convert_to_space_format: function(data) {
+    return data.replace(/(.{10})/g, '$1 ').replace(/(.{60})/g, '$1\n');
+  },
+
+  /**
+   * This is known as format type 3
+   * @param {string} data
+   * @return {string}
+   */
+  ___convert_to_number_format: function(data) {
+    //use the ___convert_to_space_format but prepend with a a number that starts with 1 and increments by 60 for each line
+    let space_format = this.___convert_to_space_format(data);
+    let space_format_lines = space_format.split('\n');
+    let number_format = '';
+    for (let i = 0; i < space_format_lines.length; i++) {
+      number_format += (i * 60 + 1) + ' ' + space_format_lines[i] + '\n';
+    }
+    return number_format;
+  }
+
+}
